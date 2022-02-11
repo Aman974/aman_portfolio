508241b2bd13c4eaf24d41d7d76e907ef01b7c6c
Aman Tiwari <80471500+aman-ama-Au17@users.noreply.github.com>
2022-02-11 15:25:42 +0530

    update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 export const projects = [
   {
-    title: "Tanishq.co.in",
+    title: "Tanishq",
     description:
       "Clone of Tanishq jewellery. Tanishq is a brand of jewellery retail stores in India. It's a fully functional clone with the both frontend and backend work",
     image: "/images/1.jpg",
@@ -10,7 +10,7 @@ export const projects = [
     id: 0,
   },
   {
-    title: "Mamaearth.in",
+    title: "Mamaearth",
     description:
       "Mamaearth is an online retailer that specializes in organic products that are best for babies, their mothers, grandparents, and others.",
     image: "/images/2.png",
