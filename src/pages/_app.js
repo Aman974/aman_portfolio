eca1223fed388d9c721b1e70d853c1b41a50aeec
Aman Tiwari <80471500+aman-ama-Au17@users.noreply.github.com>
2022-02-23 14:37:30 +0530

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import Theme from '../styles/theme';
 
+import {ScrollToTop} from "../components/ScrollToTop/ScrollToTop";
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
         <Component {...pageProps} />
+        < ScrollToTop />
       </Theme>
     </>
   );
