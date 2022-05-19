42ce805390968bc8d09bc723df2aa51c0aab610f
Aman Tiwari <80471500+aman-ama-Au17@users.noreply.github.com>
2022-05-19 11:24:28 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
-import { WritingEffect } from './WritingEffect';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+import { WritingEffect } from "./WritingEffect";
 
 const Hero = (props) => (
   <Section row nopadding>
@@ -13,13 +17,21 @@ const Hero = (props) => (
         My Personal Portfolio
       </SectionTitle>
       <SectionText>
-        <h1 style={{ color: 'magenta' }}>{`Hi, I am Aman Tiwari`}</h1>
+        <h1 style={{ color: "magenta" }}>{`Hi, I am Aman Tiwari`}</h1>
       </SectionText>
       <WritingEffect />
 
-      <Button onClick={() => window.open("https://drive.google.com/file/d/1lmyFc_qZ6oyOsyAUSAS8LRNHaNsF6ay5/view?usp=view", '_blank')}>View My Resume</Button>
+      <Button
+        onClick={() =>
+          window.open(
+            " https://drive.google.com/file/d/1tCUuSBxnkx-OVOXMhIRphDziQH4I-QY5/view?usp=view",
+            "_blank"
+          )
+        }
+      >
+        View My Resume
+      </Button>
     </LeftSection>
-
   </Section>
 );
 
